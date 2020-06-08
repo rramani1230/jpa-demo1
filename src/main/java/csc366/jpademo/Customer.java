a2387b08407d36658b7517c96250d41ad9e7d5ea
Daniel <dcjones2015@gmail.com>
2020-06-08 12:46:24 -0700

    Customer Class Push

--- src/main/java/csc366/jpademo/Customer.java
+++ src/main/java/csc366/jpademo/Customer.java
@@ -8,12 +8,46 @@ import javax.persistence.*;
 
 import javax.validation.constraints.NotNull;
 
-@Entity(name="Customer")
+@Entity
+@Table(name="Customer")
+
 public class Customer extends CompanyPerson{
+     @NotNull
+     public boolean truck_driver;
+     public int home_store;
+     public int favorite_item;
 
      public Customer (int id, String name, String email,
-     String dateOfBirth, String phoneNo) {
+     String dateOfBirth, String phoneNo, boolean truck_driver, 
+     int home_store, int favorite_item) {
           super(id,name,email,dateOfBirth,phoneNo);
+          this.truck_driver = truck_driver;
+          this.home_store = home_store;
+          this.favorite_item = favorite_item;
+     }
+
+     public boolean getTruckDriver() {
+          return this.truck_driver;
+     }
+
+     public void setTruckDriver(boolean truck_driver) {
+          this.truck_driver = truck_driver;
+     }
+
+     public int getHomeStore() {
+          return this.home_store;
+     }
+
+     public void setHomeStore(int home_store) {
+          this.home_store = home_store;
+     }
+
+     public int getFavoriteItem() {
+          return this.favorite_item;
+     }
+
+     publix void setFavoriteItem(int favorite_item) {
+          this.favorite_item = favorite_item;
      }
 
 }
