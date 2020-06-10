6ce6c13d5a0428887a3ea2be8d3bbe320ea1ec7c
wilsonh415 <wilsonh415@gmail.com>
2020-06-10 16:56:26 -0700

    fix so that master builds

--- src/main/java/csc366/jpademo/LocationManager.java
+++ src/main/java/csc366/jpademo/LocationManager.java
@@ -14,7 +14,8 @@ public class LocationManager extends Manager{
      @NotNull
      private int id;
 
-     @OneToOne(mappedBy = "storeId")
+     // @OneToOne(mappedBy = "storeId")
+     @OneToOne
      private Store store;
     
      public LocationManager (int id, String name, String email,
